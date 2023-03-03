fb2660192acb830d8f86d748dc5e51c595d87e4e
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-03 17:20:51 +0500

    Exercise 15 - Solved

---
+++ Exercise15/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const guestsList = ['Yaseen', 'Ahsan', 'Rehman'];
+console.log(`Dear ${guestsList[0]},You are cordially invited to dinner at my home on Saturday!\n`);
+console.log(`Dear ${guestsList[1]},You are cordially invited to dinner at my home on Saturday!\n`);
+console.log(`Dear ${guestsList[2]},You are cordially invited to dinner at my home on Saturday!\n`);
+console.log(`${guestsList[1]},Can\'t be able to attend the dinner. So, adding a different guest!\n`);
+guestsList.splice(1, 1, 'Zia');
+console.log(`Dear ${guestsList[0]},You are cordially invited to dinner at my home on Saturday!\n`);
+console.log(`Dear ${guestsList[1]},You are cordially invited to dinner at my home on Saturday!\n`);
+console.log(`Dear ${guestsList[2]},You are cordially invited to dinner at my home on Saturday!\n`);
