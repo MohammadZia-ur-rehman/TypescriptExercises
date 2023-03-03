7cbf91a70356f7de4fab04289813bf81169033f8
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-03 15:40:17 +0500

    Exercise 5 - Solved

---
+++ Exercise5/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Steve Jobs";
+let message = `${famous_person} once said, \"The only way to do great work is to love what you do.\"`;
+console.log(message);
---
+++ Exercise5/index.ts
@@ -0,0 +1,3 @@
+let famous_person: string = "Steve Jobs";
+let message: string = `${famous_person} once said, \"The only way to do great work is to love what you do.\"`;
+console.log(message);
