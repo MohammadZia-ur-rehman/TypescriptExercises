9d98ee72284599cabb2cea1a0bec958cef20470f
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-03 16:37:13 +0500

    Exercise 13 - Solved

---
+++ Exercise13/index.ts
@@ -0,0 +1,5 @@
+const cars: string[] = ['Civic', 'Mercedes', 'Vezel'];
+
+console.log(`I would like to own a ${cars[0]} car.`);
+console.log(`My favorite car is a ${cars[1]}.`);
+console.log(`I've always wanted to drive an ${cars[2]} car.`);
