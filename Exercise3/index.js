d4af377251e76b2125fee5262c6e342f6ae0bb1c
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-03 16:19:42 +0500

    Exercise 10 - Solved

--- Exercise3/index.js
+++ Exercise3/index.js
@@ -1,7 +1,14 @@
 "use strict";
+/*
+Program Name: String Manipulation
+Written By: Muhammad Zia Ur Rehman
+Date: 03-March-2023
+Description: The following program is used to convert the string in UPPERCASE, lowercase & Titlecase.
+*/
 let userName = "muhammad zIA Ur rehMAN";
 console.log(userName.toUpperCase());
 console.log(userName.toLowerCase());
+// toTitleCase function to convert the string in titlecase and return it (using the loop)
 function toTitleCase(str) {
     let newStr = "";
     let flag = true;
@@ -18,6 +25,7 @@ function toTitleCase(str) {
     }
     return newStr;
 }
+// toTitleCase function to convert the string in titlecase and return it (using replace function & regular expression)
 function toTitleCase1(str) {
     return str.replace(/\w\S*/g, (txt) => {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
--- Exercise3/index.ts
+++ Exercise3/index.ts
@@ -1,8 +1,16 @@
+/*
+Program Name: String Manipulation
+Written By: Muhammad Zia Ur Rehman
+Date: 03-March-2023
+Description: The following program is used to convert the string in UPPERCASE, lowercase & Titlecase.
+*/
+
 let userName: string = "muhammad zIA Ur rehMAN";
 
 console.log(userName.toUpperCase());
 console.log(userName.toLowerCase());
 
+// toTitleCase function to convert the string in titlecase and return it (using the loop)
 function toTitleCase(str:string) : string {
     let newStr: string = "";
     let flag:boolean = true;
@@ -21,6 +29,7 @@ function toTitleCase(str:string) : string {
     return newStr;
 }
 
+// toTitleCase function to convert the string in titlecase and return it (using replace function & regular expression)
 function toTitleCase1(str: string): string {
     return str.replace(/\w\S*/g, (txt: string) => {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
