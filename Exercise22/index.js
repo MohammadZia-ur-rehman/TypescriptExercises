2a757f3df112b4c9d2a49ecce611ad5e50449dd5
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-04 00:48:24 +0500

    Exercise 22 - Solved

---
+++ Exercise22/index.js
@@ -0,0 +1,6 @@
+"use strict";
+{
+    let numbers = [1, 2, 3, 4, 5];
+    //Trying to access the out-of-bound index
+    //console.log(numbers[5]);
+}
---
+++ Exercise22/index.ts
@@ -0,0 +1,6 @@
+{
+    let numbers: number[] = [1,2,3,4,5];
+
+    //Trying to access the out-of-bound index
+    //console.log(numbers[5]);
+}
