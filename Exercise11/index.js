614eb768a8c927e8b060dae9d544dc2ad6e9eaf5
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-03 16:26:37 +0500

    Exercise 11 - Solved

---
+++ Exercise11/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ["fahad", "hasnat", "hamid"];
+for (let loop = 0; loop < names.length; loop++) {
+    console.log(`${names[loop]} is my friend`);
+}
---
+++ Exercise11/index.ts
@@ -0,0 +1,5 @@
+let names: string [] = ["fahad", "hasnat", "hamid"];
+
+for(let loop=0; loop<names.length ; loop++){
+    console.log(`${names[loop]} is my friend`);
+}
