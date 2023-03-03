95fb54dd05fbbc4350323f4cf610d1e861543cac
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-03 17:09:25 +0500

    Exercise 14 - Solved

---
+++ Exercise14/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const guestList = ['Muhammad', 'Zia', 'Rehman'];
+console.log(`Dear ${guestList[0]},\nYou are cordially invited to dinner at my home on Saturday!\n`);
+console.log(`Dear ${guestList[1]},\nYou are cordially invited to dinner at my home on Saturday!\n`);
+console.log(`Dear ${guestList[2]},\nYou are cordially invited to dinner at my home on Saturday!\n`);
---
+++ Exercise14/index.ts
@@ -0,0 +1,7 @@
+const guestList: string[] = ['Muhammad', 'Zia', 'Rehman'];
+
+console.log(`Dear ${guestList[0]},\nYou are cordially invited to dinner at my home on Saturday!\n`);
+
+console.log(`Dear ${guestList[1]},\nYou are cordially invited to dinner at my home on Saturday!\n`);
+
+console.log(`Dear ${guestList[2]},\nYou are cordially invited to dinner at my home on Saturday!\n`);
