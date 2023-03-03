b9bd37068896467f6e07d92938da313f5e20bb81
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-03 15:50:36 +0500

    Exercise 6 - Solved

---
+++ Exercise6/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const userName = "\t\n  John Doe \n\t";
+console.log(`Name with whitespace: \"${userName}\"`);
+console.log(`Name without whitespace: "${userName.trim()}"`);
---
+++ Exercise6/index.ts
@@ -0,0 +1,5 @@
+const userName: string = "\t\n  John Doe \n\t";
+
+console.log(`Name with whitespace: \"${userName}\"`);
+
+console.log(`Name without whitespace: "${userName.trim()}"`);
