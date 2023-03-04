d5ca6ed131e2ad1047724da34355c6c24f784d39
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-04 23:41:53 +0500

    Exercise 30 - Solved

---
+++ Exercise30/index.ts
@@ -0,0 +1,12 @@
+{
+    const usernames = ['john', 'mary', 'admin', 'david', 'jane'];
+
+    for (let i = 0; i < usernames.length; i++){
+        if (usernames[i] === 'admin') {
+            console.log('Hello admin, would you like to see a status report?');
+        }
+        else{
+            console.log(`Hello ${usernames[i]}, thank you for logging in`);
+        }
+    }
+}
