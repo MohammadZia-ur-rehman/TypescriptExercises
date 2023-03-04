3d1310d336d10808ab826bc4acb380ae9486a66e
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-05 00:28:02 +0500

    Exercise 36 - Solved

---
+++ Exercise36/index.js
@@ -0,0 +1,7 @@
+"use strict";
+{
+    function MakeShirt(size, text) {
+        console.log(`The size of shirt iss \"${size}\" & \"${text}\" should be printed on it.`);
+    }
+    MakeShirt(36, "I love coding");
+}
---
+++ Exercise36/index.ts
@@ -0,0 +1,6 @@
+{
+    function MakeShirt(size:number, text:string) : void{
+        console.log(`The size of shirt iss \"${size}\" & \"${text}\" should be printed on it.`);
+    }
+    MakeShirt(36,"I love coding");
+}
