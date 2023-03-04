562a74b30be44781ce976ee3c629d7b96f41bd23
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-04 23:11:16 +0500

    Exercise 27 - Solved

---
+++ Exercise27/index.js
@@ -0,0 +1,42 @@
+"use strict";
+{
+    let alien_color = 'green';
+    if (alien_color === 'green') {
+        console.log('The player earned 5 points for shooting the green alien!');
+    }
+    else if (alien_color === 'yellow') {
+        console.log('The player earned 10 points for shooting the yellow alien!');
+    }
+    else if (alien_color === 'red') {
+        console.log('The player earned 15 points for shooting the red alien!');
+    }
+    else {
+        console.log('The player didn\'t shoot any aliens!');
+    }
+    alien_color = 'yellow';
+    if (alien_color === 'green') {
+        console.log('The player earned 5 points for shooting the green alien!');
+    }
+    else if (alien_color === 'yellow') {
+        console.log('The player earned 10 points for shooting the yellow alien!');
+    }
+    else if (alien_color === 'red') {
+        console.log('The player earned 15 points for shooting the red alien!');
+    }
+    else {
+        console.log('The player didn\'t shoot any aliens!');
+    }
+    alien_color = 'red';
+    if (alien_color === 'green') {
+        console.log('The player earned 5 points for shooting the green alien!');
+    }
+    else if (alien_color === 'yellow') {
+        console.log('The player earned 10 points for shooting the yellow alien!');
+    }
+    else if (alien_color === 'red') {
+        console.log('The player earned 15 points for shooting the red alien!');
+    }
+    else {
+        console.log('The player didn\'t shoot any aliens!');
+    }
+}
