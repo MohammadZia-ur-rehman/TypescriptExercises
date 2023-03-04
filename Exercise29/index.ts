0a9335b96cce5fb6bbfd740f8d2271d4ff1d19af
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-04 23:36:56 +0500

    Exercise 29 - Solved

---
+++ Exercise29/index.ts
@@ -0,0 +1,23 @@
+{
+    const favoriteFruits: string[] = ["banana", "apple", "mango"];
+
+    if (favoriteFruits.includes("banana")) {
+        console.log("You really like bananas!");
+    }
+
+    if (favoriteFruits.includes("apple")) {
+        console.log("You really like apples!");
+    }
+
+    if (favoriteFruits.includes("mango")) {
+        console.log("You really like mangoes!");
+    }
+
+    if (favoriteFruits.includes("pear")) {
+        console.log("You really like pears!");
+    }
+
+    if (favoriteFruits.includes("kiwi")) {
+        console.log("You really like kiwis!");
+    }
+}
