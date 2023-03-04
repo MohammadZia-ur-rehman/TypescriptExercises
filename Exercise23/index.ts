9afe88e67cc6ad1a9070bce53caad84a73e6069b
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-04 22:22:54 +0500

    Exercise 23 - Solved

---
+++ Exercise23/index.ts
@@ -0,0 +1,47 @@
+let num1 = 5;
+let num2 = 10;
+console.log("Is num1 < num2? I predict True.");
+console.log(num1 < num2);
+
+let str1 = 'hello';
+let str2 = 'world';
+console.log("Is str1 not equal to str2? I predict True.");
+console.log(str1 !== str2);
+
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3];
+console.log("Are arr1 and arr2 equal? I predict False.");
+console.log(arr1 == arr2);
+
+console.log("Does str1 contain the substring 'lo'? I predict True.");
+console.log(str1.includes('lo'));
+
+let num3 = 10;
+let num4 = '10';
+console.log("Is num3 equal to num4? I predict True.");
+console.log(num3 === parseInt(num4));
+
+let str3 = 'hello';
+let str4 = 'Hello';
+console.log("Are str3 and str4 equal? I predict False.");
+console.log(str3 === str4);
+
+let arr3 = [1, 2, 3];
+let arr4 = [3, 2, 1];
+console.log("Is arr3 not equal to arr4? I predict True.");
+console.log(arr3 != arr4);
+
+let bool3 = true;
+let bool4 = true;
+console.log("Are bool3 and bool4 equal? I predict True.");
+console.log(bool3 === bool4);
+
+let num5 = 5;
+let num6 = 10;
+console.log("Is num5 greater than num6? I predict False.");
+console.log(num5 > num6);
+
+let str5 = 'cat';
+let str6 = 'dog';
+console.log("Is str5 greater than str6? I predict False.");
+console.log(str5 > str6);
