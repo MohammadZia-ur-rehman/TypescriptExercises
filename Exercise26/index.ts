d016f97e3f1b95808c73d2e17f7bc25604dc367a
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-04 23:03:26 +0500

    Exercise 26 - Solved

---
+++ Exercise26/index.ts
@@ -0,0 +1,19 @@
+{
+    let alien_color = 'green';
+
+    if (alien_color === 'green') {
+        console.log('The player just earned 5 points for shooting the green alien!');
+    }
+    else {
+        console.log('The player just earned 10 points for shooting a non-green alien!');
+    }
+
+    alien_color = 'yellow';
+
+    if (alien_color === 'green') {
+        console.log('The player just earned 5 points for shooting the green alien!');
+    }
+    else {
+        console.log('The player just earned 10 points for shooting a non-green alien!');
+    }
+}
