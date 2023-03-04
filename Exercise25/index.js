fafcd16f2c708da21594e6c2e8965e052095474e
Muhammad Zia Ur Rehman <mohammadzia199424@gmail.com>
2023-03-04 22:59:59 +0500

    Exercise 25 - Solved

---
+++ Exercise25/index.js
@@ -0,0 +1,12 @@
+"use strict";
+{
+    let alien_color = 'green';
+    if (alien_color === 'green') {
+        console.log('The player just earned 5 points!');
+    }
+    // Failing version
+    alien_color = 'yellow';
+    if (alien_color === 'green') {
+        console.log('The player just earned 5 points!');
+    }
+}
---
+++ Exercise25/index.ts
@@ -0,0 +1,14 @@
+{
+    let alien_color = 'green';
+
+    if (alien_color === 'green') {
+    console.log('The player just earned 5 points!');
+    }
+
+    // Failing version
+    alien_color = 'yellow';
+
+    if (alien_color === 'green') {
+    console.log('The player just earned 5 points!');
+    }
+}
